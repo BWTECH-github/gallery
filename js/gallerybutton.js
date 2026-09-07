de5b3dbb21fa6933f14177e85f37fe2594c84762
GrossLukas <l.gross@bw.tech>
2026-09-07 15:33:08 +0200

    fix(gallery): Absturz und Tastaturzugang des Galerie-Knopfes
    Der Knopf baute seine Adresse aus dem globalen FileList. In den Ansichten
    ohne Verzeichnis (Favoriten, geteilte Dateien, Tags, Papierkorb) gibt es
    dieses Objekt nicht mehr, der Aufruf landete beim nativen window.FileList
    des Browsers und brach mit "getCurrentDirectory is not a function" ab.
    Die Dateiliste kommt jetzt von OCA.Files.App.getCurrentFileList(); laesst
    sich keine Adresse bilden, blendet sich der Knopf aus statt auf "null" zu
    springen.
    
    Ausserdem war der Knopf ein div mit Klick-Behandlung und damit ohne
    Tastatur erreichbar. Er hat jetzt role=button, einen Tabulatorstopp,
    eine Beschriftung und reagiert auf Eingabe- und Leertaste.

--- js/gallerybutton.js
+++ js/gallerybutton.js
@@ -9,15 +9,49 @@ GalleryButton.url = null;
  */
 GalleryButton.onFileListUpdated = function () {
 	"use strict";
-	var fileList;
+	var fileList = GalleryButton.getFileList();
 
-	if (GalleryButton.isPublic) {
-		fileList = OCA.Sharing.PublicApp.fileList;
-	} else {
-		fileList = FileList;
+	// Die Sonderansichten (Favoriten, geteilte Dateien, Tags, Papierkorb) fuehren
+	// kein Verzeichnis. Ohne diese Pruefung landet der Aufruf beim nativen
+	// window.FileList des Browsers und bricht mit einem TypeError ab.
+	if (!fileList || typeof fileList.getCurrentDirectory !== 'function') {
+		GalleryButton.url = null;
+		GalleryButton.toggleButton(false);
+		return;
 	}
 
 	GalleryButton.buildGalleryUrl(fileList.getCurrentDirectory().replace(/^\//, ''));
+	GalleryButton.toggleButton(true);
+};
+
+/**
+ * Returns the file list of the view which is currently shown
+ */
+GalleryButton.getFileList = function () {
+	"use strict";
+
+	if (GalleryButton.isPublic) {
+		return (OCA.Sharing && OCA.Sharing.PublicApp) ? OCA.Sharing.PublicApp.fileList : null;
+	}
+
+	// Der Kern haelt die Liste der aktiven Ansicht bereit; das alte globale
+	// FileList gibt es nur noch in der Standardansicht.
+	if (OCA.Files && OCA.Files.App && typeof OCA.Files.App.getCurrentFileList === 'function') {
+		return OCA.Files.App.getCurrentFileList();
+	}
+
+	return (typeof FileList !== 'undefined') ? FileList : null;
+};
+
+/**
+ * Hides the button while no gallery URL could be built
+ */
+GalleryButton.toggleButton = function (visible) {
+	"use strict";
+
+	if (GalleryButton.button && typeof GalleryButton.button.toggle === 'function') {
+		GalleryButton.button.toggle(!!visible);
+	}
 };
 
 /**
@@ -58,15 +92,29 @@ $(document).ready(function () {
 			$('#fileList').on('updated', GalleryButton.onFileListUpdated);
 
 			// Button for opening files list as gallery view
+			// Rolle, Tabulatorstopp und Beschriftung machen den Knopf mit der
+			// Tastatur bedienbar; das Bild selbst bleibt dekorativ.
 			GalleryButton.button =
-				$('<div id="gallery-button" class="button view-switcher">' +
+				$('<div id="gallery-button" class="button view-switcher" role="button" tabindex="0">' +
 						'<div id="button-loading"></div>' +
 					'<img class="svg" src="' + OC.imagePath('core', 'actions/toggle-pictures.svg') +
 					'"' +
-					'alt="' + t('gallery', 'Gallery view') + '"/>' +
+					'alt=""/>' +
 					'</div>');
+			GalleryButton.button.attr('aria-label', t('gallery', 'Gallery view'));
+			GalleryButton.button.attr('title', t('gallery', 'Gallery view'));
 
-			GalleryButton.button.click(function () {
+			GalleryButton.button.on('click keydown', function (event) {
+				if (event.type === 'keydown' &&
+						event.key !== 'Enter' && event.key !== ' ' && event.key !== 'Spacebar') {
+					return;
+				}
+				event.preventDefault();
+
+				if (!GalleryButton.url) {
+					return;
+				}
+
 				$(this).children('#button-loading').addClass('loading');
 				window.location.href = GalleryButton.url;
 			});
